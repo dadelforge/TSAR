f18e6353fac49bf9e863aa2df656765ca89da652
dadelforge <dam.delforge@gmail.com>
2017-11-14 15:33:32 +0100

    Add some docstrings

--- tvp/base.py
+++ tvp/base.py
@@ -5,15 +5,26 @@ And that the variable is numeric
 
 """
 
-# TODO: implement forgiveness for empty series and df
-# TODO: implement is numeric
-
 import inspect
+
 import pandas as pd
 from pandas.core.generic import NDFrame
 
-# import user defined exception
-from tvpexept import *
+
+# User defined exceptions
+class TVPError(Exception):
+    """Base class for tvp errors"""
+    pass
+
+
+class NotMonotonicIncreasingError(TVPError):
+    """Raised when a datetime index is not monotonic increasing"""
+    pass
+
+
+class NotFixedFrequencyError(TVPError):
+    """Raised when a datetime index has no fixed frequency"""
+    pass
 
 
 class TVPBase(NDFrame):
@@ -36,11 +47,11 @@ class TVPBase(NDFrame):
 
         # check if datetime index is monotonic increasing
         if not index.is_monotonic_increasing:
-            raise NotMonotonicIncreasingIndex
+            raise NotMonotonicIncreasingError('Index in not monotonic increasing')
 
         # check if datetime index has fixed frequency
         if index.inferred_freq is None:
-            raise NotFixedFrequencyIndex
+            raise NotFixedFrequencyError('Index has no fixed frequency')
 
         return NDFrame.__new__(cls, *args, **kwargs)
 
@@ -81,6 +92,59 @@ class TVPSeries(TVPBase, pd.Series):
     This class constraints pandas series to have a monotonic 
     increasing pd.DatetimeIndex with fixed frequency.
     
+    TVPSeries are instantiated as a pandas.Series
+    
+    _.
+        
+    Examples
+    --------
+    
+    A valid example:
+    
+    >>> date_range = pd.date_range(start='2010-06-15', periods=10)
+    >>> values = range(10)
+    >>> ts = TVPSeries(data=values, index=date_range)
+    
+    Using a integer type index:
+    
+    >>> int_range = range(10)
+    >>> values =  range(10)
+    >>> ts = TVPSeries(data=values, index=int_range)
+    Traceback (most recent call last):
+        ...
+    TypeError: Index is not type DatetimeIndex
+    
+    Using a non monotonic increasing DatetimeIndex:
+    
+    >>> date_range = pd.date_range(start='2010-06-15', periods=10)
+    >>> inv_date_range = date_range[::-1]
+    >>> values = range(10)
+    >>> ts = TVPSeries(data=values, index=inv_date_range)
+    Traceback (most recent call last):
+        ...
+    NotMonotonicIncreasingError: Index in not monotonic increasing
+    
+    Using a non fixed frequency DatetimeIndex:
+    
+    >>> date_range = pd.date_range(start='2010-06-15', periods=10)
+    >>> broken_date_range = date_range.drop(pd.to_datetime('2010-06-18'))
+    >>> values = range(9)
+    >>> ts = TVPSeries(data=values, index=broken_date_range)
+    Traceback (most recent call last):
+        ...
+    NotFixedFrequencyError: Index has no fixed frequency
+    
+    Raises
+    ------
+    
+    TypeError
+        If index is not type `pandas.DatetimeIndex`
+    
+    NotMonotonicIncreasingError
+        If index is not monotonic increasing
+        
+    NotFixedFrequencyError
+        If index has no fixed frequency    
     """
 
     @property
@@ -107,3 +171,9 @@ class TVPDataFrame(TVPBase, pd.DataFrame):
     @property
     def _constructor_sliced(self):
         return TVPSeries
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
