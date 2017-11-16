931e269b1bb8bf1635bc44c13ce9538437fc3495
dadelforge <dam.delforge@gmail.com>
2017-11-16 16:45:17 +0100

    Add automutual information to correlate.py

--- tsar/correlate.py
+++ tsar/correlate.py
@@ -1,18 +1,24 @@
 """Python functions to measure dependencies 
 
-We distinguish self time delayed dependencies
 
 """
+
+#TODO: Add top level documentation
 
 import numpy as np
 import pandas as pd
 import tsar
 from tsar.dtypes import is_1darray_like
+from sklearn.metrics import mutual_info_score, \
+    normalized_mutual_info_score
+
+# ----------------------------------------------------------------------
+# Self dependency functions
 
 def autocorrelation(ts, maxlag = 20):
-    """Autocorrelation function
+    """Auto Correlation Function
     
-    The autocorrelation function is a metric of linear self dependence.
+    The auto correlation function is a metric of linear self dependence.
     
     The pearson moment of correlation is applied to the time series and
     its multiple lags.
@@ -23,10 +29,11 @@ def autocorrelation(ts, maxlag = 20):
     
     Parameters
     ----------
-    ts : 1d array like
-        TVPSeries or array-like
+    ts : 1d array_like
+        Array like holding the time series values. Valid types are list of 
+        number, dict of number values, numpy 1d array or pandas Series.
     maxlag : int
-        Maximum lag to compute autocorrelation
+        Maximum lag to compute autocorrelation.
 
     Returns
     -------
@@ -37,13 +44,16 @@ def autocorrelation(ts, maxlag = 20):
     --------
     
     >>> ts = tsar.data.lorenz()['x']
-    >>> ac_ts = autocorrelation(ts, maxlag=10)
-    >>> print len(ac_ts)
+    >>> rho = autocorrelation(ts, maxlag=10)
+    >>> print len(rho)
     11
     
     Raises
     ------
-    
+    TypeError
+        Raised if input is not one dimensional numeric.
+    IndexError
+        Raised if maxlag greater than time series length.
     
     """
 
@@ -59,13 +69,136 @@ def autocorrelation(ts, maxlag = 20):
 
         raise IndexError('Maximum lag {} is greater than series length {}'.format(maxlag, len(ts)))
 
+    # list of lags
+
     lags = range(maxlag+1)
+
+    # conversion of ts into a pd.Series
 
     ts = pd.Series(ts)
 
     autocorr = [ts.autocorr(i) for i in lags]
 
     return autocorr
+
+def automutualinfo(ts, maxlag=20, bins='sqrt'):
+    """
+    
+    Parameters
+    ----------
+    ts : 1d array_like
+        Array like holding the time series values. Valid types are list of 
+        number, dict of number values, numpy 1d array or pandas Series.
+    maxlag : int
+        Maximum lag to compute auto mutual information.
+    bins : int or sequence of scalars or str, optional
+        If `bins` is an int, it defines the number of equal-width
+        bins in the given range. If `bins` is a sequence, it defines 
+        the bin edges, including the rightmost edge, allowing for 
+        non-uniform bin widths.
+        .. versionadded:: numpy 1.11.0
+        If `bins` is a string from the list below, `histogram` will use
+        the method chosen to calculate the optimal bin width and
+        consequently the number of bins (see [3] for more detail on
+        the estimators) from the data that falls within the requested
+        range. While the bin width will be optimal for the actual data
+        in the range, the number of bins will be computed to fill the
+        entire range, including the empty portions. For visualisation,
+        using the 'auto' option is suggested. Weighted data is not
+        supported for automated bin size selection.
+        'auto'
+            Maximum of the 'sturges' and 'fd' estimators. Provides good
+            all around performance.
+        'fd' (Freedman Diaconis Estimator)
+            Robust (resilient to outliers) estimator that takes into
+            account data variability and data size.
+        'doane'
+            An improved version of Sturges' estimator that works better
+            with non-normal datasets.
+        'scott'
+            Less robust estimator that that takes into account data
+            variability and data size.
+        'rice'
+            Estimator does not take variability into account, only data
+            size. Commonly overestimates number of bins required.
+        'sturges'
+            R's default method, only accounts for data size. Only
+            optimal for gaussian data and underestimates number of bins
+            for large non-gaussian datasets.
+        'sqrt'
+            Square root (of data size) estimator, used by Excel and
+            other programs for its speed and simplicity. This is 
+            default.
+
+    Returns
+    -------
+    automi : list
+        List holding auto mutual information values up to maxlag.
+        
+    Examples
+    --------
+    
+    >>> ts = tsar.data.lorenz()['x']
+    >>> ami = automutualinfo(ts, maxlag=20)
+    >>> print ami
+
+    References
+    ----------
+    
+    [1] Sklearn: Mutual Information between two clusterings
+        http://scikit-learn.org/stable/modules/generated/sklearn.metrics.mutual_info_score.html
+    [2] numpy.histogram2d
+        https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.histogram2d.html
+    [3] numpy.histogram
+        https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.histogram.html
+    
+    """
+
+    # test for one-dimensional object
+
+    if not is_1darray_like(ts):
+
+        raise TypeError('Input object should be 1 dimensional numeric array like object.')
+
+    # test for Index error
+
+    if maxlag >= len(ts):
+
+        raise IndexError('Maximum lag {} is greater than series length {}'.format(maxlag, len(ts)))
+
+    # conversion of ts into a pd.Series
+
+    ts = pd.Series(ts)
+
+    # list of lags
+
+    lags = range(maxlag + 1)
+
+    # use numpy histogram to get bins
+
+    bin_edges = np.histogram(ts.values, bins=bins)[1]
+
+    # define auto mutual information list
+    automi = []
+
+    for t in lags:
+
+        ts_lag = ts.shift(t)
+
+        # use numpy histogram2d to build the contigency matrix
+
+        contigency_xy = np.histogram2d(ts, ts_lag, bin_edges)[0]
+
+        # compute mutual information
+
+        mi = mutual_info_score(None, None, contingency=contigency_xy)
+
+        # append result to list
+
+        automi.append(mi)
+
+    return automi
+
 
 if __name__=='__main__':
     import doctest
